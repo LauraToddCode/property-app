ec272f0242e16849cfbd067b535df9d068202cd9
LauraToddCode <laura20todd@gmail.com>
2021-02-18 14:10:35 +0000

    Unpdated Listview layout

--- src/Components/ListView/ListViewCard.js
+++ src/Components/ListView/ListViewCard.js
@@ -1,8 +1,17 @@
 import React from "react";
 
-function ListViewCard() {
+function ListViewCard(props) {
     return (
-        <div>all the house details</div>
+        <div class="listViewCardCont">
+            <div class="listViewImgCont">
+                <img src={props.mainImg} alt={props.imgAlt} class="listViewPropertyImg"/>
+                <p class="listViewPrice">{props.price}</p>
+            </div>
+            <div class="listViewInfoCont">
+                <h3>{props.cardTitle}</h3>
+                <p>{props.propertyDesc}</p>
+            </div>
+        </div>
     )
 }
 
