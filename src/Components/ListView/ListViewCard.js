cfa5753b105f6d28e3c4a3a0fbcd066238778fdd
LauraToddCode <laura20todd@gmail.com>
2021-02-20 17:50:35 +0000

    Update ListViewCard.js

--- src/Components/ListView/ListViewCard.js
+++ src/Components/ListView/ListViewCard.js
@@ -2,14 +2,29 @@ import React from "react";
 
 function ListViewCard(props) {
     return (
-        <div class="listViewCardCont">
-            <div class="listViewImgCont">
-                <img src={props.mainImg} alt={props.imgAlt} class="listViewPropertyImg"/>
-                <p class="listViewPrice">{props.displayPrice}</p>
+        <div className="listViewCardCont">
+            <div className="listViewImgCont">
+                <img src={props.mainImg} alt={props.imgAlt} className="listViewPropertyImg"/>
+                <p className="listViewPrice">{props.displayPrice}</p>
             </div>
-            <div class="listViewInfoCont">
-                <h3>{props.cardTitle}</h3>
+            <div className="listViewInfoCont">
+                <h3>{props.bedrooms} bed {props.type} for sale</h3>
+                <div className="roomTypes">
+                    <div className="indiRoomType">
+                        <img src={process.env.PUBLIC_URL + "/images/propertyCards/bedroom.png"} />
+                        <p>{props.bedrooms}</p>
+                    </div>
+                    <div className="indiRoomType">
+                        <img src={process.env.PUBLIC_URL + "/images/propertyCards/bathroom.png"} />
+                        <p>{props.bathrooms}</p>
+                    </div>
+                    <div className="indiRoomType">
+                        <img src={process.env.PUBLIC_URL + "/images/propertyCards/livingRoom.png"} />
+                        <p>{props.livingRooms}</p>
+                    </div>
+                </div>
                 <p>{props.propertyDesc}</p>
+                <a className="findOutMore">Find out more</a>
             </div>
         </div>
     )
