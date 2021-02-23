2f1f50e18bf65e6393696399e31a957af0995070
LauraToddCode <laura20todd@gmail.com>
2021-02-23 12:23:49 +0000

    Updated ToggleViewNav styling

--- src/Components/ToggleViewNav.js
+++ src/Components/ToggleViewNav.js
@@ -1,50 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-class ToggleViewNav extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            ListViewSelected: true,
-            MapViewSelected: false,
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick = (event) => {
-        if (event.target.id == "listViewLink") {
-            this.setState({
-                ListViewSelected: true,
-                MapViewSelected: false
-            })
-        } else if (event.target.id == "mapViewLink") {
-            this.setState({
-                ListViewSelected: false,
-                MapViewSelected: true
-            })
-        }
-        console.log("list view selected:" + this.state.ListViewSelected)
-        console.log("map view selected:" + this.state.MapViewSelected)
-    }
-
-    render() {
-        const { ListViewSelected, MapViewSelected } = this.state
-        const listTabClass = ListViewSelected == true ? "tabFocus" : "tabBlur"
-        const mapTabClass = MapViewSelected == true ? "tabFocus" : "tabBlur"
-
-
-
-        return (
-            <div className="navToggles">
-                <div className={listTabClass + " navTab"} id="listViewLink" onClick={this.handleClick}>
-                    <Link to="/property-app" className="navToggleLinks">List View</Link>
-                </div>
-                <div className={mapTabClass + " navTab"} id="mapViewLink" onClick={this.handleClick}>
-                    <Link to="/map-view" className="navToggleLinks">Map View</Link>
-                </div>
-            </div>
-        )
-    }
+function ToggleViewNav() {
+    return (
+        <div className="navToggles">
+            <NavLink to="/property-app" activeClassName="active" className="navTab">
+                <p>List View</p>
+            </NavLink>
+            <NavLink to="/map-view" activeClassName="active" className="navTab">
+                <p>Map View</p>
+            </NavLink>
+        </div>
+    )
 }
 
 export default ToggleViewNav
