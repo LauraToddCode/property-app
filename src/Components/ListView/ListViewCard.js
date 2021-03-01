e884f5893e5e76ef755c0bc6d9e0d27077393e38
LauraToddCode <laura20todd@gmail.com>
2021-03-01 18:18:43 +0000

    Used Context to pass state of clicked button to PropertyProfile

--- src/Components/ListView/ListViewCard.js
+++ src/Components/ListView/ListViewCard.js
@@ -3,36 +3,38 @@ import Paper from '@material-ui/core/Paper';
 import BathtubIcon from '@material-ui/icons/Bathtub';
 import KingBedIcon from '@material-ui/icons/KingBed';
 import WeekendIcon from '@material-ui/icons/Weekend';
+import { Link } from "react-router-dom";
 
 function ListViewCard(props) {
 
     return (
         <Paper elevation={3} className="listViewCardCont">
-
-                <div elevation={3} className="listViewImgCont">
-                    <img src={props.mainImg} alt={props.imgAlt} className="listViewPropertyImg"/>
-                    <p className="listViewPrice">{props.displayPrice}</p>
-                </div>
-                <div elevation={3} className="listViewInfoCont">
-                    <h3>{props.bedrooms} bed {props.type} for sale</h3>
-                    <div className="roomTypes">
-                        <div className="indiRoomType">
-                            <KingBedIcon />
-                            <p>{props.bedrooms}</p>
-                        </div>
-                        <div className="indiRoomType">
-                            <BathtubIcon />
-                            <p>{props.bathrooms}</p>
-                        </div>
-                        <div className="indiRoomType">
-                            <WeekendIcon />
-                            <p>{props.livingRooms}</p>
-                        </div>
+            <div elevation={3} className="listViewImgCont">
+                <img src={props.mainImg} alt={props.imgAlt} className="listViewPropertyImg"/>
+                <p className="listViewPrice">{props.displayPrice}</p>
+            </div>
+            <div elevation={3} className="listViewInfoCont">
+                <h3>{props.bedrooms} bed {props.type} for sale</h3>
+                <div className="roomTypes">
+                    <div className="indiRoomType">
+                        <KingBedIcon />
+                        <p>{props.bedrooms}</p>
                     </div>
-                    <p>{props.propertyDesc}</p>
-                    <a className="findOutMore" href="/property-profile">Find out more</a>
+                    <div className="indiRoomType">
+                        <BathtubIcon />
+                        <p>{props.bathrooms}</p>
+                    </div>
+                    <div className="indiRoomType">
+                        <WeekendIcon />
+                        <p>{props.livingRooms}</p>
+                    </div>
                 </div>
-            </Paper>
+                <p>{props.propertyDesc}</p>
+                <Link to="/property-profile" >
+                    <button id={props.id} className="findOutMore" onClick={props.onClick}>Find out more</button>
+                </Link>
+            </div>
+        </Paper>
 
     )
 }
--- src/ListView.js
+++ src/ListView.js
@@ -3,7 +3,7 @@ import propertiesData from "./properties.json";
 import ListViewCard from "./Components/ListView/ListViewCard";
 import ToggleViewNav from "./Components/ToggleViewNav";
 import Filters from "./Components/Filters";
-import MortgageCalculator from "./Components/MortgageCalculator";
+import { ListingsContext } from "./ListingsContext";
 
 class ListView extends React.Component {
     constructor(props) {
@@ -15,10 +15,10 @@ class ListView extends React.Component {
             minBedrooms: 1,
             maxBedrooms: 10,
         }
-        this.handleMinPriceChange = this.handleMinPriceChange.bind(this)
-        this.handleMaxPriceChange = this.handleMaxPriceChange.bind(this)
-        this.handleMinBedChange = this.handleMinBedChange.bind(this)
-        this.handleMaxBedChange = this.handleMaxBedChange.bind(this)
+        this.handleMinPriceChange = this.handleMinPriceChange.bind(this);
+        this.handleMaxPriceChange = this.handleMaxPriceChange.bind(this);
+        this.handleMinBedChange = this.handleMinBedChange.bind(this);
+        this.handleMaxBedChange = this.handleMaxBedChange.bind(this);
     }
 
     handleMinPriceChange = (event) => {
@@ -54,14 +54,41 @@ class ListView extends React.Component {
 
         return (
             <div className="appContainer">
-                <MortgageCalculator propertyPrice="200000"/>
-                <Filters handleMinPriceChange={this.handleMinPriceChange} handleMaxPriceChange={this.handleMaxPriceChange} handleMinBedChange={this.handleMinBedChange} handleMaxBedChange={this.handleMaxBedChange} minPriceValue={minPrice} maxPriceValue={maxPrice} minBedroomsValue={minBedrooms} maxBedroomsValue={maxBedrooms}/>
+                <Filters 
+                    handleMinPriceChange={this.handleMinPriceChange} 
+                    handleMaxPriceChange={this.handleMaxPriceChange} 
+                    handleMinBedChange={this.handleMinBedChange} 
+                    handleMaxBedChange={this.handleMaxBedChange} 
+                    minPriceValue={minPrice} 
+                    maxPriceValue={maxPrice} 
+                    minBedroomsValue={minBedrooms} 
+                    maxBedroomsValue={maxBedrooms}
+                />
                 <ToggleViewNav />
+                
                 <div className="MuiPaper-elevation3 listViewList">
                     {properties.map(property => validProperty(property.price, property.bedrooms) && (
-                        <ListViewCard mainImg={process.env.PUBLIC_URL + "/images/properties/" + property.mainImg} imgAlt={property.imgAlt} displayPrice={property.displayPrice} type={property.type} propertyDesc={property.propertyDesc} bedrooms={property.bedrooms} bathrooms={property.bathrooms} livingRooms={property.livingRooms} key={property.id} />
+                        <ListingsContext.Consumer>
+                            {(context) => (
+                                <ListViewCard 
+                                    id={property.id}
+                                    mainImg={process.env.PUBLIC_URL + "/images/properties/" + property.mainImg} 
+                                    imgAlt={property.imgAlt} 
+                                    displayPrice={property.displayPrice} 
+                                    type={property.type} 
+                                    propertyDesc={property.propertyDesc} 
+                                    bedrooms={property.bedrooms} 
+                                    bathrooms={property.bathrooms} 
+                                    livingRooms={property.livingRooms} 
+                                    key={property.id}
+                                    onClick={context.getId}
+                                />
+                            )}
+                        </ListingsContext.Consumer>
                     ))}
+                    
                 </div>
+                
             </div>
         )
     }
