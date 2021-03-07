15045630981a0675837fbb868ffe09f8b6ad9fc4
LauraToddCode <laura20todd@gmail.com>
2021-03-07 15:14:05 +0000

    Added underline animation to saved properties header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 
-function Header() {
+function Header(props) {
     return (
         <header id="header">
-            <a href="/property-app" className="headerIcon">
-                <HomeOutlinedIcon 
-                    style={{ fontSize: 80 }}
-                />
-            </a>
-            <h1>Prime Pad</h1>
+            <div id="title">
+                <a href="/property-app" className="headerIcon">
+                    <HomeOutlinedIcon 
+                        style={{ fontSize: 80 }}
+                    />
+                </a>
+                <h1>Prime Pad</h1>
+            </div>
+            <div id="savedLinkCont">
+                <Link to="/saved-properties" id="headerSaved">
+                    Saved Properties ({props.savedItems.length - 1})
+                </Link>
+            </div>
+            
         </header>
     )
 }
