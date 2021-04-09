4ac1092a542589a0673705ac59178bee32f1398b
LauraToddCode <laura20todd@gmail.com>
2021-04-09 17:36:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch, BrowserRouter as Router } from "react-router-dom";
 import ListView from "./Components/ListView";
 import MapView from "./Components/MapView";
 import PropertyProfile from "./Components/PropertyProfile";
@@ -11,21 +11,26 @@ import { connect } from "react-redux";
 function App({ currentItem }) {
 
     return (
-        <div>
-            <Header/>
-                <Route exact path="/property-app" component={ ListView } />
-                <Route exact path="/map-view" component={ MapView } />
-                {!currentItem ? (
-                    <Redirect to="/property-app" />
-                ): (
-                    <Route exact path="/property-profile/:id" component= { PropertyProfile } />
-                )}
-                <Route exact path="/saved" component= { Saved } />
-        </div>
+        <Router>
+            <div>
+                <Header/>
+                    <Switch>
+                        <Route exact path="/property-app" component={ ListView } />
+                        <Route exact path="/map-view" component={ MapView } />
+                        {!currentItem ? (
+                            <Redirect to="/property-app" />
+                        ): (
+                            <Route exact path="/property-profile/:id" component= { PropertyProfile } />
+                        )}
+                        <Route exact path="/saved" component= { Saved } />
+                    </Switch>
+            </div>
+        </Router>
     )
 }
 
 const mapStateToProps = state => {
+    console.log(state)
     return {
         currentItem: state.save.currentItem
     }
