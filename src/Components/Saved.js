40244ee5ac0db26b07baeb39ba18769c889f7ce4
LauraToddCode <laura20todd@gmail.com>
2021-04-11 11:43:06 +0100

    Update Saved.js
    Added Back Button and message if no properties are saved

--- src/Components/Saved.js
+++ src/Components/Saved.js
@@ -1,10 +1,15 @@
 import React from "react";
 import SavedItem from "./SavedItem";
+import BackBtn from "./BackBtn";
 import { connect } from "react-redux";
 
 function Saved({ savedProperties }) {
+    const noItems = savedProperties.length == 0 ? "show" : "hide";
+
     return (
-        <div>
+        <div id="savedContainer">
+            <BackBtn />
+            <p className={"savedMessage " + noItems}>You have no saved properties</p>
             {savedProperties.map(item =>
                 <SavedItem key={item.id} itemData={item}/>
             )}
