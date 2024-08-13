6edf73162d5d76afe7028b93f89832597b6ca7e8
LauraToddCodes <74312582+LauraToddCodes@users.noreply.github.com>
2024-08-13 17:46:01 +0100

    Remove fontsource-roboto dependency

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import 'fontsource-roboto';
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
